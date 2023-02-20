6f323435f0a22e90b3c662044202d72739ec2f1a
ranggatw <rangga.public@gmail.com>
2023-02-20 12:52:25 +0700

    pokemon profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const profile = JSON.parse(localStorage.getItem("userData"));
+  const pokemonList = JSON.parse(localStorage.getItem("pokemonHave"));
+  console.log(pokemonList);
+  console.log(profile.username);
+  return (
+    <div>
+      <h3>Profile</h3>
+      <div>
+        <div>username : {profile.username}</div>
+        <h5>Pokemon You Have: </h5>
+        <div style={{}}>
+          {pokemonList.map((pokemon, index) => (
+            <div key={index}>
+              <div>Pokemon name : {pokemon.name}</div>
+              {/* <div>Pokemon name : {pokemon.name}</div>
+              <div>Pokemon name : {pokemon.name}</div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
